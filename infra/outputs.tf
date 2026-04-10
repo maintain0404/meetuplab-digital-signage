e4fc92ad5ca846a7207e0351b3424f7ab418587f
Taein Min <maintain0404@gmail.com>
2026-04-11 06:06:21 +0900

    feat: Add Firebase Hosting PR preview

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -17,3 +17,9 @@ output "firebase_hosting_url" {
   description = "Firebase Hosting URL"
   value       = "https://${google_firebase_hosting_site.default.site_id}.web.app"
 }
+
+output "github_actions_service_account_key" {
+  description = "GitHub Secret FIREBASE_SERVICE_ACCOUNT에 등록할 값"
+  value       = base64decode(google_service_account_key.github_actions.private_key)
+  sensitive   = true
+}
